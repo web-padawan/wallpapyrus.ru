d5c796b70580b4ec00777c3726b8645dc982c86e
web-padawan <skamandr@yandex.ru>
2015-11-04 17:24:49 +0200

    [new] wallpaper template: override image field, output download links

---
+++ themes/wallpapyrus/templates/field--field_wallpaper_image--wallpaper.tpl.php
@@ -0,0 +1,28 @@
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="field-items"<?php print $content_attributes; ?>>
+    <?php foreach ($items as $delta => $item): ?>
+
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+        <?php
+          $url = $item['#item']['uri'];
+          $image = array(
+            'style_name' => 'wallpaper_preview_480x300',
+            'path' => $url
+          );
+
+          print theme('image_style', $image);
+
+        ?>
+        <div class="download">
+          <div class="download__title">Select your size:</div>
+          <div class="downoad__links">
+            <?php
+              print l('Thumbnail', file_force_create_url(image_style_path('thumbnail', $url)));
+            ?>
+          </div>
+        </div>
+      </div>
+
+    <?php endforeach; ?>
+  </div>
+</div>
