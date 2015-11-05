1fcf07666126c47dcd21c45dbc13481b03c3b6bf
web-padawan <skamandr@yandex.ru>
2015-11-05 08:19:19 +0200

    [mod] wallpaper image field - change markup, classes and styles

--- themes/wallpapyrus/templates/field--field_wallpaper_image--wallpaper.tpl.php
+++ themes/wallpapyrus/templates/field--field_wallpaper_image--wallpaper.tpl.php
@@ -1,39 +1,43 @@
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="field-items"<?php print $content_attributes; ?>>
-    <?php foreach ($items as $delta => $item): ?>
+<?php foreach ($items as $delta => $item): ?>
 
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+  <?php if ($element['#view_mode'] === 'teaser'): ?>
+    <div class="wallp wallp--teaser">
+  <?php else: ?>
+    <div class="wallp wallp--full">
+  <?php endif; ?>
 
-        <?php if ($element['#view_mode'] === 'teaser'):?>
-          <a href="/node/<?php print $element['#object']->nid ?>">
-        <?php endif ?>
+    <?php
+      $node = $element['#object'];
+    ?>
 
-        <?php
-          $url = $item['#item']['uri'];
-          $image = array(
-            'style_name' => 'wallpaper_preview_480x300',
-            'path' => $url
-          );
-          print theme('image_style', $image);
-        ?>
+    <?php if ($element['#view_mode'] === 'teaser'): ?>
+      <a class="wallp__link" href="/node/<?php print $node->nid ?>" title="<?php print $node->title ?>">
+    <?php endif; ?>
 
-        <?php if ($element['#view_mode'] === 'teaser'):?>
-          </a>
-        <?php endif ?>
+    <?php
+      $url = $item['#item']['uri'];
+      $image = array(
+        'style_name' => 'wallpaper_preview_480x300',
+        'path' => $url
+      );
+      print theme('image_style', $image);
+    ?>
 
-        <?php if ($element['#view_mode'] === 'full'): ?>
-          <div class="download">
-            <div class="download__title">Select your size:</div>
-            <div class="downoad__links">
-              <?php
-                print l('Thumbnail', file_force_create_url(image_style_path('thumbnail', $url)));
-              ?>
-            </div>
-          </div>
-        <?php endif ?>
+    <?php if ($element['#view_mode'] === 'teaser'): ?>
+        <div class="wallp__title"><?php print $node->title ?></div>
+      </a>
+    <?php endif; ?>
 
+    <?php if ($element['#view_mode'] === 'full'): ?>
+      <div class="wallp__download wallp-download">
+        <div class="wallp-download__title">Select your size:</div>
+        <div class="wallp-download__link">
+          <?php
+            print l('Thumbnail', file_force_create_url(image_style_path('thumbnail', $url)));
+          ?>
+        </div>
       </div>
+    <?php endif; ?>
 
-    <?php endforeach; ?>
-  </div>
-</div>
+    </div>
+<?php endforeach; ?>
