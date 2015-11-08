bcafad4d1f5ddc8526eb8a788c8c281dc9e7363b
web-padawan <skamandr@yandex.ru>
2015-11-08 09:34:59 +0200

    [mod] messages: custom styles

--- themes/wallpapyrus/layouts/default/default-layout.tpl.php
+++ themes/wallpapyrus/layouts/default/default-layout.tpl.php
@@ -50,16 +50,29 @@
     <div class="main__container">
       <div class="content" role="main">
         <?php print render($page['highlighted']); ?>
+
         <a id="main-content"></a>
+
         <?php print render($title_prefix); ?>
+
         <?php if ($title): ?>
+        <div class="content__row">
           <h1 class="content__title"><?php print $title; ?></h1>
+        </div>
         <?php endif; ?>
+
         <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
+
+        <?php if ($messages): ?>
+        <div class="content__row">
+          <?php print $messages; ?>
+        </div>
+        <?php endif; ?>
+
         <?php if ($action_links): ?>
           <ul class="action-links"><?php print render($action_links); ?></ul>
         <?php endif; ?>
+
         <?php print render($page['content']); ?>
       </div>
 
