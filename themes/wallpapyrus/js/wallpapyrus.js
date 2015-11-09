433f14e3116830ddc8c68dee7280f8a955cac829
web-padawan <skamandr@yandex.ru>
2015-11-09 18:25:41 +0200

    [mod] check wallpaper type when generating url

--- themes/wallpapyrus/js/wallpapyrus.js
+++ themes/wallpapyrus/js/wallpapyrus.js
@@ -1,11 +1,17 @@
 (function($){
   $(function() {
 
+    // wallpaper type
+    var type = $('.wallp-download').attr('data-type');
+
     // available styles
-    var styles = [
+    var desktop = [
         'wide_2560x1600', 'wide_1920x1200', 'wide_1680x1050', 'wide_1440x900',
         'hd_2560x1440', 'hd_1920x1080', 'hd_1600x900', 'hd_1366x768',
-        'full_1600x1200', 'full_1400x1050', 'full_1280x960', 'full_1024x768',
+        'full_1600x1200', 'full_1400x1050', 'full_1280x960', 'full_1024x768'
+    ];
+
+    var mobile = [
         'apple_320x480', 'apple_640x960', 'apple_640x1136', 'apple_750x1334',
         'apple_768x1024', 'apple_1080x1920', 'apple_1536x2048',
         'android_480x800', 'android_480x854', 'android_540x960', 'android_600x1024',
@@ -14,6 +20,7 @@
 
     // check if we have appropriate image style
     var userScreen = screen.width + 'x' + screen.height,
+        styles = (type === 'desktop') ? desktop : mobile;
         userMatch = styles.filter(function(value) {
           return (~value.indexOf(userScreen)) ? true: false;
         });
