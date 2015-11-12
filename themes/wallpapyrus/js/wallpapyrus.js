351a1e9882cfb3e81a5e1da6689efd4086cb0f08
web-padawan <skamandr@yandex.ru>
2015-11-12 16:37:49 +0200

    [mod] wallpaper page: field label titles

--- themes/wallpapyrus/js/wallpapyrus.js
+++ themes/wallpapyrus/js/wallpapyrus.js
@@ -35,5 +35,23 @@
       $('.wallp-download__user-size').text(userScreen);
     }
 
+    // filд wallpaper fields titles
+    var $date = $('.submitted');
+    $date.attr('title', 'Добавлено: ' + $date.text());
+
+    var $download = $('.field--name-field-download-count');
+    $download.attr('title', $download.text());
+
+    var $category = $('.field--name-field-tag-categories');
+    $category.attr('title', $category.text());
+
+    var $tags = $('.field--name-field-tags');
+    $tags.attr('title', $tags.text());
+
+    var $colors = $('.wallp-colors__label');
+    $colors.attr('title', 'Основные цвета');
+
+    var $rating = $('.field--name-field-wallpaper-rating');
+    $rating.attr('title', 'Рейтинг');
   });
 })(jQuery);
