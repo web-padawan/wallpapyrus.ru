ca803a6a2487a2b96a143e4e85788b2c5ffe2cb2
web-padawan <skamandr@yandex.ru>
2015-11-21 11:55:15 +0200

    [fix] remove wrong check

--- modules/wallpapyrus_downloads/wallpapyrus_downloads.js
+++ modules/wallpapyrus_downloads/wallpapyrus_downloads.js
@@ -4,17 +4,12 @@
 
       $('.wallp-download').find('a').on('click', function() {
 
-        if (typeof settings['ajaxLoading'] === 'undefined' || settings.ajaxLoading === false) {
-          settings.ajaxLoading = true;
-
-          $.ajax({
-            url: 'wallpapyrus_downloads/' + Drupal.settings.nid,
-            success: function(data) {
-              $('.field--name-field-download-count .field__item').html(data);
-              settings.ajaxLoading = false;
-            }
-          });
-        }
+        $.ajax({
+          url: 'wallpapyrus_downloads/' + Drupal.settings.nid,
+          success: function(data) {
+            $('.field--name-field-download-count .field__item').html(data);
+          }
+        });
 
       });
     }
